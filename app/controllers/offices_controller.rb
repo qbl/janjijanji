c2aa1e6a21928f2a01f4480cabf768f86d373408
Iqbal Farabi <farabi.iqbal@gmail.com>
2017-06-10 07:31:21 +0700

    Remove dates from Office

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -69,6 +69,6 @@ class OfficesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def office_params
-      params.require(:office).permit(:name, :start_date, :thru_date)
+      params.require(:office).permit(:name)
     end
 end
